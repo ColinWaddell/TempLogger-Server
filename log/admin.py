8c7320a8ada7aa16e07f76265142c8ed09cee05a
Colin Waddell <mrcolin@gmail.com>
2018-01-08 19:17:06 +0000

    better admin view

--- log/admin.py
+++ log/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import TemperatureSensor, Reading
 
+class ReadingAdmin(admin.ModelAdmin):
+    model = Reading
+    list_display = (
+        'sensor',
+        'datetime',
+        'temperature_c',
+    )
+
 admin.site.register(TemperatureSensor)
-admin.site.register(Reading)
+admin.site.register(Reading, ReadingAdmin)

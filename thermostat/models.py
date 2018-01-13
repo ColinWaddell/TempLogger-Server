97abb0bc2d582e7857b06b0e6ac017a349136420
Colin Waddell <mrcolin@gmail.com>
2018-01-13 12:54:13 +0000

    actually deactivating program

--- thermostat/models.py
+++ thermostat/models.py
@@ -36,7 +36,7 @@ class Program(models.Model):
         self.save()
 
     def deactivate(self):
-        self.activate = False
+        self.active = False
         self.save()
             
 
@@ -92,18 +92,15 @@ class Thermostat(models.Model):
         self.save()
 
     def too_warm(self):
-        print ("too warm" if self.get_temperature() > self.target else "too cold")
         return self.get_temperature() > self.target
 
     def switch_on(self):
-        print("switch on")
         device = devices.HANDLERS[self.device]()
         device.switch_on()
         self.on = True
         self.save()
 
     def switch_off(self):
-        print("switch off")
         dev = devices.HANDLERS[self.device]()
         dev.switch_off()
         self.on = False
--- thermostat/programs.py
+++ thermostat/programs.py
@@ -13,19 +13,14 @@ def _program(thermostat):
     program = thermostat.program
 
     for action in thermostat.get_actions():
-        print("for actions")
         if program.active_day and action.active_time():
-            print("active day times")
             thermostat.test()
             if not program.active:
-                print("not program active")
                 program.activate()
                 thermostat.set_target(action.target)
                 return
         else:
-            print("else")
             if program.active:
-                print("program active")
                 program.deactivate()
                 thermostat.switch_off()
                 return

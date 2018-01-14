52fc3f1c43b4cde69ab715c03933b20ca8067338
Colin Waddell <mrcolin@gmail.com>
2018-01-14 16:54:55 +0000

    correct message

--- thermostat/management/commands/update-thermostats.py
+++ thermostat/management/commands/update-thermostats.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from thermostat.models import Thermostat
 
 class Command(BaseCommand):
-    help = 'Test all the thermostats status'
+    help = 'Test all the thermostats for status updates'
 
     def handle(self, *args, **options):
         _ = [thermostats.update() for thermostats in Thermostat.objects.all()]
